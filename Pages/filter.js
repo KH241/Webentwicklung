57b51a82a4dc8c5c0d5f3a03c6e650b2c85da63c
KH241 <k.hurt@gmx.de>
2021-12-05 11:01:28 +0100

    finished assignment 2

--- Pages/filter.js
+++ Pages/filter.js
@@ -2,7 +2,7 @@ var table = document.getElementById("studentTable");
 var department = document.getElementById("DepartmentSelect");
 var semester = document.getElementById("SemesterSelect");
 var tr = table.getElementsByTagName("tr");
-
+var summer = [4,5,6,7,8,9];
 
 function filterDepartment(){
     for(i = 0; i < tr.length; i++){
@@ -22,6 +22,25 @@ function filterDepartment(){
 }
 
 function filterSemester(){
+    for(i = 0; i < tr.length; i++){
+        d = tr[i].getElementsByTagName("td")[5];
+        if(d){    
+            if(semester.value != "All"){
+                s = parseInt(d.innerHTML.split("-")[1]);
+                console.log(s);
 
+                if(summer.includes(s)){ s = "Summer" }
+                else { s = "Winter" }
+
+                if(semester.value == s){
+                    tr[i].style.display = "";
+                }else{
+                    tr[i].style.display = "none";
+                }
+            }else{
+                tr[i].style.display = "";
+            }
+        }
+    }
 }
 
